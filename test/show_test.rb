37dc1ed6a438cf26063e90c1d4fadc67be7faa02
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-11 09:27:36 -0700

    Create test for total salary

--- test/show_test.rb
+++ test/show_test.rb
@@ -23,12 +23,16 @@ class CellTest < Minitest::Test
     assert_equal [michael_knight, kitt], knight_rider.characters
   end
 
+  def test_it_costs_a_lot_of_money
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
+
 end
 
-
-# pry(main)> knight_rider.total_salary
-# # => 2600000
-#
 # pry(main)> knight_rider.highest_paid_actor
 # # => "David Hasselhoff"
 #
