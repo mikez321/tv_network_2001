8a8a06bdd43741f43d1703a40867bbe76aedd6c4
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-11 09:08:52 -0700

    Create character class

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,19 @@
+class Character
+  attr_reader :info
+  def initialize(info)
+    @info = info
+  end
 
+  def name
+    info[:name]
+  end
+
+  def actor
+    info[:actor]
+  end
+
+  def salary
+    info[:salary]
+  end
+
+end
