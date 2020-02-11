d7cd284b8d642ca902470fa3deaa34fad56121d3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-11 09:53:24 -0700

    Create network test

---
+++ test/network_test.rb
@@ -0,0 +1,22 @@
+require './lib/character'
+require './lib/show'
+require './lib/network'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class NetworkTest < Minitest::Test
+
+  def test_it_exists
+    nbc = Network.new("NBC")
+
+    assert_instance_of Network, nbc
+  end
+
+  def test_it_has_attributes
+    nbc = Network.new("NBC")
+
+    assert_equal "NBC", nbc.name
+    assert_equal [], nbc.shows
+  end
+
+end
