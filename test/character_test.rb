d6c6ad3697a5f60301c16db91014a0d37cd9b822
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-11 09:47:55 -0700

    Name character test properly

--- test/character_test.rb
+++ test/character_test.rb
@@ -2,7 +2,7 @@ require './lib/character'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class CellTest < Minitest::Test
+class CharacterTest < Minitest::Test
 
   def test_it_exists
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
